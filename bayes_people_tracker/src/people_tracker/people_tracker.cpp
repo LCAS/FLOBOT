5f3c3a0d2a9bdb4ce9a0657809555205432d912d
Zhi Yan <yz@ai.univ-paris8.fr>
2017-03-17 12:04:42 +0000

    Add files via upload

--- bayes_people_tracker/src/people_tracker/people_tracker.cpp
+++ bayes_people_tracker/src/people_tracker/people_tracker.cpp
@@ -316,29 +316,6 @@ void PeopleTracker::publishDetections(people_msgs::People msg) {
   pub_people.publish(msg);
 }
 
-//@todo analyze vels law for kids or animals tracking.
-void PN_experts(geometry_msgs::PoseArray &variance, geometry_msgs::PoseArray &velocity, geometry_msgs::PoseArray &trajectory) {
-  float path_length = 0.0;
-  for(int i = 1; i < trajectory.poses.size(); i++) {
-    path_length += hypot(trajectory.poses[i].position.x-trajectory.poses[i-1].position.x,
-			 trajectory.poses[i].position.y-trajectory.poses[i-1].position.y);
-  }
-  float sum_velocity = 0.0;
-  for(int i = 0; i < velocity.poses.size(); i++)
-    sum_velocity += fabs(velocity.poses[i].position.x+velocity.poses[i].position.y);
-  float avg_velocity = sum_velocity / velocity.poses.size();
-  float sum_variance = 0.0;
-  for(int i = 0; i < variance.poses.size(); i++)
-    sum_variance += variance.poses[i].position.x+variance.poses[i].position.y;
-  float avg_variance = sum_variance / variance.poses.size();
-  //std::cerr << "path_length = " << path_length << ", avg_velocity = " << avg_velocity << ", avg_variance = " << avg_variance << std::endl;
-  // @todo rosparams: pexpert_min_dist_, pexpert_min_vel_, pexpert_max_vel_, nexpert_min_var
-  if(path_length >= 2.0 && avg_velocity >= 0.2 && avg_velocity <= 1.4)
-    trajectory.header.frame_id = "human_trajectory";
-  if(path_length < 0.2 && avg_velocity < 0.2 && avg_variance < 0.02)
-    trajectory.header.frame_id = "static_trajectory";
-}
-
 void PeopleTracker::publishTrajectory(std::vector<geometry_msgs::Pose> poses,
 				      std::vector<geometry_msgs::Pose> vels,
 				      std::vector<geometry_msgs::Pose> vars,
@@ -384,12 +361,6 @@ void PeopleTracker::publishTrajectory(std::vector<geometry_msgs::Pose> poses,
   /*** add new coming poses to the previous_poses list ***/  
   for(int i = 0; i < poses.size(); i++) {
     bool new_pose = true;
-    for(int j = 0; j < previous_poses.size(); j++) {
-      if(poses[i].position.z >= 0.0 && boost::get<3>(previous_poses[j]).position.z == poses[i].position.z) {
-	new_pose = false;
-	break;
-      }
-    }
     if(new_pose) {
       //if(vars[i].position.x+vars[i].position.y < 1.0)
       previous_poses.push_back(boost::make_tuple(pids[i], vars[i], vels[i], poses[i]));
@@ -466,7 +437,6 @@ std::vector<double> PeopleTracker::cartesianToPolar(geometry_msgs::Point point) 
 
 void PeopleTracker::detectorCallback(const geometry_msgs::PoseArray::ConstPtr &pta, std::string detector) {
   //std::cerr << "[people_tacker] got " << pta->poses.size() << " poses, from " << detector << std::endl;
-  
   // Publish an empty message to trigger callbacks even when there are no detections.
   // This can be used by nodes which might also want to know when there is no human detected.
   if(pta->poses.size() == 0) {
